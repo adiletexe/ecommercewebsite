b73e1f925e7c5c747b483dbe43b64bf65381d536
adiletexe <108264958+adiletexe@users.noreply.github.com>
2022-08-31 16:47:12 +0600

    copying frontend and creating base template

--- ecommercedjango/settings.py
+++ ecommercedjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -114,8 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ]
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
