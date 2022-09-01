be7adb6734f5caaed55baf5f9eb324a618d5489d
adiletexe <108264958+adiletexe@users.noreply.github.com>
2022-09-01 19:27:23 +0600

    creating categories model commit

---
+++ categories/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Categories(models.Model):
+    title = models.CharField(max_length=40, unique=True)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
