7c35db93e70106c54b6b4bea32121b68b1bca196
adiletexe <108264958+adiletexe@users.noreply.github.com>
2022-09-04 11:03:55 +0600

    cannot change password

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import Accounts
+
+class AdminPage(UserAdmin):
+    list_display = ('username', 'firstname', 'secondname', 'date_signed', 'date_logged', 'is_active')
+    list_display_links = ('username', 'firstname', 'secondname')
+    ordering = ('-date_signed',)
+    readonly_fields = ('date_signed', 'date_logged', 'is_active')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 # Register your models here.
-admin.site.register(Accounts)
+admin.site.register(Accounts, AdminPage)
