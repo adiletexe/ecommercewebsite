404d30b355bbabccfc863a7e3d8d23637c23b331
adiletexe <108264958+adiletexe@users.noreply.github.com>
2022-09-06 23:04:24 +0600

    just for views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.filter(is_available=True)
+    dictionary = {
+        'products':products
+    }
+    return render(request, 'home.html', dictionary)
